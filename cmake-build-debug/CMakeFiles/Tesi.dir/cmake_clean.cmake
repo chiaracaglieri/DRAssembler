9844dca35148cbb37b531b1d6fc779bec98e8c7d
Chiara Caglieri <chiaracaglieri@gmail.com>
2018-07-22 12:07:26 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Tesi.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tesi.dir/main.cpp.o"
+  "Tesi.pdb"
+  "Tesi.exe"
+  "libTesi.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tesi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
